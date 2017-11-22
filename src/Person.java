e469cbe7d9b15fec28c76f27b346754f3550a27f
johan1031 <johan.carrby@gmail.com>
2017-11-22 13:22:45 +0100

    Seperats the Fullname to last name and first name

--- src/Person.java
+++ src/Person.java
@@ -3,8 +3,22 @@ public class Person {
 
 	private String fname;
 	private String lname;
+	private int index; 
 	
-
+public Person(String Fullname){
+		
+		char[] cArr = Fullname.toCharArray();
+		
+		for(int i = 0; i < cArr.length; i++){
+			if(cArr[i] == ' '){
+				index = i;
+			}
+		}
+		this.fname = Fullname.substring(index);
+		this.lname = Fullname.substring(0, index);
+		
+	}
+	
 	public String getFname() {
 		return fname;
 	}
